d4ab509b55e6dbcb611ddede0fe63e72fc7013d5
ttisseve <50583758+ttisseve@users.noreply.github.com>
2019-06-06 19:35:03 -0400

    Add files via upload

---
+++ JS/main.js
@@ -0,0 +1,11 @@
+/* Set the width of the side navigation to 250px */
+function openNav() {
+  document.getElementById("topbar").style.height = "100px";
+  document.getElementById("main").style.marginTop = "100px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("topbar").style.height = "0";
+  document.getElementById("main").style.marginTop = "0";
+}
